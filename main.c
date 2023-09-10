e58ce544a8cb01e32478c8feabd7b1a06db84d3c
Ali-Mohamed-cairo <alimohamed98321@gmail.com>
2023-09-10 04:43:28 +0300

    First use

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#\
+define \
+test \
+ 33
+
+
+
+int main(void){
+    
+    printf("%d\n", sizeof(int));
+    if(128U == (-1))//  0000 0000 0000 0000 0000 0000 0000 0000 0100 > 1111 1111 1111 1111 1111 1111 1111 1111
+        printf("true");
+    else
+        printf("false");
+    int x;
+    getchar();
+    return 0;
+}
